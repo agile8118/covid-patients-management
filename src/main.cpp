1feb3d9210bcaff8aaddc4a8ebf30e3edae5cc26
Joseph Heidari <agile.8272@gmail.com>
2021-02-14 12:29:53 +0330

    Adds the ability to remove a patient's records

--- src/main.cpp
+++ src/main.cpp
@@ -97,38 +97,36 @@ void add_new_patience(std::vector<Patient> *patients)
     (*patients).push_back(p);
 }
 
-void display_patient_records(std::string ssn, const std::vector<Patient> *patients)
+void display_patient_records(Patient patient)
 {
-    for (const Patient &patient : *patients)
+
+    if (patient.getSSN() == "0000000000")
+        return;
+
+    std::cout << "--------------------------------" << std::endl;
+    std::cout << "Information of patient with the SSN number of " + patient.getSSN() << std::endl;
+    std::cout << "First Name: " + patient.getFirstName() << std::endl;
+    std::cout << "Last Name: " + patient.getLastName() << std::endl;
+    std::cout << "Age: " + std::to_string(patient.getAge()) << std::endl;
+
+    std::string hasUnderlyingHealthProblems = patient.getHasUnderlyingHealthProblems() ? "Yes" : "No";
+    std::cout << "Has underlying health problems: " + hasUnderlyingHealthProblems << std::endl;
+
+    std::cout << "Gender: " + patient.getGender() << std::endl;
+    std::cout << "Admission Date: " + patient.getAdmissionDate() << std::endl;
+    if (patient.hasDischarged())
     {
-        if (patient.getSSN() == ssn)
-        {
-            std::cout << "--------------------------------" << std::endl;
-            std::cout << "Information of patient with the SSN number of " + patient.getSSN() << std::endl;
-            std::cout << "First Name: " + patient.getFirstName() << std::endl;
-            std::cout << "Last Name: " + patient.getLastName() << std::endl;
-            std::cout << "Age: " + std::to_string(patient.getAge()) << std::endl;
-
-            std::string hasUnderlyingHealthProblems = patient.getHasUnderlyingHealthProblems() ? "Yes" : "No";
-            std::cout << "Has underlying health problems: " + hasUnderlyingHealthProblems << std::endl;
-
-            std::cout << "Gender: " + patient.getGender() << std::endl;
-            std::cout << "Admission Date: " + patient.getAdmissionDate() << std::endl;
-            if (patient.hasDischarged())
-            {
-                std::cout << "Discharged Date: " + patient.getDischargedDate() << std::endl;
-            }
-
-            if (patient.hasPassedAway())
-            {
-                std::cout << "Death Date: " + patient.getDeathDate() << std::endl;
-            }
-            std::cout << "--------------------------------" << std::endl;
-        }
+        std::cout << "Discharged Date: " + patient.getDischargedDate() << std::endl;
     }
+
+    if (patient.hasPassedAway())
+    {
+        std::cout << "Death Date: " + patient.getDeathDate() << std::endl;
+    }
+    std::cout << "--------------------------------" << std::endl;
 }
 
-void edit_patience_records(std::string ssn, std::vector<Patient> *patients)
+void edit_patient_records(std::string ssn, std::vector<Patient> *patients)
 {
     for (Patient &patient : *patients)
     {
@@ -212,10 +210,28 @@ void edit_patience_records(std::string ssn, std::vector<Patient> *patients)
     }
 }
 
-//void delete_patience(string ssn) {
-//
-//}
-//
+void display_all_patients(const std::vector<Patient> *patients)
+{
+    for (const Patient &patient : *patients)
+    {
+        display_patient_records(patient);
+    }
+}
+
+void delete_patient_records(std::string ssn, std::vector<Patient> *patients)
+{
+    for (int i = 0; i < patients->size(); ++i)
+    {
+        if (patients->operator[](i).getSSN() == ssn)
+        {
+            patients->erase(patients->begin() + i);
+            std::cout << "--------------------------------" << std::endl;
+            std::cout << "Patient's records with SSN of " + patients->operator[](i).getSSN() + " was successfully removed." << std::endl;
+            std::cout << "--------------------------------" << std::endl;
+        }
+    }
+}
+
 //void display_hospitalized_patiences_records() {
 //
 //}
@@ -276,13 +292,14 @@ int main()
         std::cout << " [2] Show a patient's records" << std::endl;
         std::cout << " [3] Edit a patient's records" << std::endl;
         std::cout << " [4] Delete a patient's records" << std::endl;
-        std::cout << " [5] Show all patients' records currently in hospitalization " << std::endl;
-        std::cout << " [6] Show all patients' records who have passed away " << std::endl;
-        std::cout << " [7] Show all patients' records who have discharged " << std::endl;
-        std::cout << " [8] Show all patients' records ordered by hospitalization duration" << std::endl;
-        std::cout << " [9] Percentage of dead patients who had underlying health problems and were over 70" << std::endl;
-        std::cout << "[10] Male to female deaths ratio" << std::endl;
-        std::cout << "[11] Exit" << std::endl;
+        std::cout << " [5] Show all patient's records" << std::endl;
+        std::cout << " [6] Show all patients' records currently in hospitalization " << std::endl;
+        std::cout << " [7] Show all patients' records who have passed away " << std::endl;
+        std::cout << " [8] Show all patients' records who have discharged " << std::endl;
+        std::cout << " [9] Show all patients' records ordered by hospitalization duration" << std::endl;
+        std::cout << " [10] Percentage of dead patients who had underlying health problems and were over 70" << std::endl;
+        std::cout << "[11] Male to female deaths ratio" << std::endl;
+        std::cout << "[12] Exit" << std::endl;
 
         std::cout << "Please choose an option: ";
         if (!(std::cin >> option))
@@ -303,15 +320,31 @@ int main()
             std::cout << "---Showing a patient's records---" << std::endl;
             std::cout << "Enter the patient's SSN: ";
             std::cin >> ssn;
-            display_patient_records(ssn, &patients);
+            for (Patient &patient : patients)
+            {
+                if (patient.getSSN() == ssn)
+                {
+                    display_patient_records(patient);
+                }
+            }
             break;
         case 3:
             std::cout << "---Editing a patient's records---" << std::endl;
             std::cout << "Enter the patient's SSN: ";
             std::cin >> ssn;
-            edit_patience_records(ssn, &patients);
+            edit_patient_records(ssn, &patients);
             break;
-        case 11:
+        case 4:
+            std::cout << "---Deleteing a patient's records---" << std::endl;
+            std::cout << "Enter the patient's SSN: ";
+            std::cin >> ssn;
+            delete_patient_records(ssn, &patients);
+            break;
+        case 5:
+            std::cout << "---Showing all patient's records---" << std::endl;
+            display_all_patients(&patients);
+            break;
+        case 12:
             std::cout << "Exiting out of the application..." << std::endl;
             exit = true;
             break;
